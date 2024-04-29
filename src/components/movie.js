85838382cba44c5582edfc815c6f5a3901c3ff67
raffaelli28 <raffaelli.ispas@ucdenver.edu>
2024-04-28 18:33:19 -0600

    modified:   src/components/movie.js 	deleted:    yarn.lock

--- src/components/movie.js
+++ src/components/movie.js
@@ -3,6 +3,11 @@ import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux'
 import { fetchMovie } from "../actions/movieActions";
 import MovieDetail from "../components/moviedetail"
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { BsPinAngle } from 'react-icons/bs';
+import {Glyphicon, Panel} from 'react-bootstrap';
+import {Image} from 'react-bootstrap';
 
 // support routing
 
