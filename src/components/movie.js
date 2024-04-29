aafd8fa132f7b253530064760b729ae13949c087
raffaelli28 <raffaelli.ispas@ucdenver.edu>
2024-04-28 18:48:05 -0600

    modified:   src/components/movie.js

--- src/components/movie.js
+++ src/components/movie.js
@@ -10,7 +10,7 @@ import {Glyphicon, Panel} from 'react-bootstrap';
 import {Image} from 'react-bootstrap';
 
 // support routing
-
+/*
 function Movie(props) {
     const [selectedMovie] = useState(props.selectedMovie);
     const params = useParams();
@@ -23,5 +23,64 @@ function Movie(props) {
 
     return (<MovieDetail movieId={movieId} />)
 }
+*/
 
-export default Movie;
+class Movie extends Component {
+    componentDidMount() {
+        const {dispatch} = this.props;
+        if (this.props.selectedMovie == null){
+            dispatch(fetchMovie(this.props.movieId));
+        }
+    }
+
+    render() {
+        const ActorInfo = ({actors}) => {
+            return actors.map((actor, i) =>
+                <p key={i}>
+                    <b>{actor.actorName}</b> {actor.characterName}
+                </p>
+        )
+        }
+
+        const ReviewInfo = ({reviews}) => {
+            return reviews.map((review, i) =>
+                <p key={i}>
+                    <b>{review.username}</b> {review.review}
+                    <Glyphicon glyph={'start'} /> {review.rating}
+                </p>
+        )
+        }
+
+        const DetailInfo = ({currentMovie}) => {
+            if (!currentMovie) {
+                return <div>Loading...</div>;
+            }
+            return(
+                <Panel>
+                    <Panel.Heading>Movie Detail</Panel.Heading>
+                    <Panel.Body><Image className="image" src={currentMovie.imageUrl} thumbnail/></Panel.Body>
+                    <ListGroup>
+                        <ListGroupItem>{currentMovie.title}</ListGroupItem>
+                        <ListGroupItem><ActorInfo actors={currentMovie.actors} /></ListGroupItem>
+                        <ListGroupItem><h4><Glyphicon glyph={'star'}/> {currentMovie.avgRating}</h4></ListGroupItem>
+                    </ListGroup>
+                    <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
+                </Panel>
+            );
+        }
+        return (
+            <DetailInfo currentMovie={this.props.selectedMovie} />
+        )
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps);
+    return {
+        selectedMovie: state.movie.selectedMovie,
+        movieId: ownProps.match.paramas.movieId
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Movie));
+// export default Movie;
